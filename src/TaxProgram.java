d41ce6b88439eeb8d24740818ffef5b00fc8f859
RenieG <igakaho@yahoo.com>
2022-02-20 10:28:25 -0600

    Create tax program

--- src/TaxProgram.java
+++ src/TaxProgram.java
@@ -11,14 +11,22 @@ public class TaxProgram {
         2. Ask user for their taxable income
         3. Accept user income
         4. Store the user's input
-        5. Calculate income after tax (NewIncome)
-        6. Print the new income
+        5. Calculate tax only
+        6. Calculate income after tax (NewIncome)
+        7. Print the new income
          */
 
         double income, tax, newIncome;
 
         System.out.println("Please enter your taxable income:");
         Scanner userinput=new Scanner(System.in);
+
+        income = userinput.nextDouble();
+        tax = income * 0.13;
+        newIncome = income - tax;
+
+        System.out.println("income(before tax);" + income);
+        System.out.println("income(after tax);" + newIncome);
     }
 
 }
